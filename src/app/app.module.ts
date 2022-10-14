935b926bfb0e0f23030e6842f09f8a616677ee25
Mona Lisa <nancyema4848@gmail.com>
2022-10-13 19:35:57 -0600

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  edad: number;
+}
+interface TuSeniora {
+  nombre: string;
+  descripcion: string;
+}
+
+const personas: Persona[] = [
+  {
+    nombre: "Agustin",
+    edad: 4
+  },
+  {
+    nombre: "Juan",
+    edad: 40
+  },
+  {
+    nombre: "Mauricio",
+    edad: 10
+  },
+  {
+    nombre: "Mario alias gorreado",
+    edad: 22
+  }
+]
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent  {
+  numero:number =0;
+  lista:Persona[]=[];
+
+  constructor(){
+    this.numero = 0;
+    this.lista = personas
+  }
+  
+  reemplazarEdad(nombre:string, edad:number):void {
+    const listaPersonas = this.lista
+    const reemplazarEdad = listaPersonas.map(
+      (persona) => persona.nombre === nombre ? ({...persona, edad: edad}) : persona
+    )
+
+    this.lista = reemplazarEdad
+  }
+
+  agregarUnAnioDeEdad(nombre:string, edad: number):void{
+    this.reemplazarEdad(nombre, edad+1)
+  }
+
+  quitarUnAnioDeEdad(nombre:string, edad: number):void{
+    this.reemplazarEdad(nombre, edad-1)
+  }
+
+  numero1():void{
+    this.numero+=1
+  }
+  numero2():void{
+    this.numero-=1
+  }    
+  anuncios:TuSeniora[]=[{
+    nombre:"cuca",
+    descripcion:"mis nalgas"
+
+  },
+  {
+    nombre:"cuca",
+    descripcion:"mis nalgas"
+
+  },
+  {
+    nombre:"cuca",
+    descripcion:"mis nalgas"
+
+  }
+  ]
+
+}
+
+
+
